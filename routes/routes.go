8a1140980c146a128751b4c83210c0a4d1ee9bb7
Eugene Martynov <tambov.j678@gmail.com>
2025-02-10 14:58:45 +0300

    Remove redundant comments from route definitions and add openapi specs

--- routes/routes.go
+++ routes/routes.go
@@ -8,29 +8,29 @@ import (
 
 func SetupRoutes(movieHandler *handlers.MovieHandler, showtimeHandler *handlers.ShowtimeHandler, authHandler *handlers.AuthHandler, reservationHandler *handlers.ReservationHandler) {
 	// Movie routes
-	http.HandleFunc("/movies", movieHandler.HandleGetMovies)                                                       // GET /movies
-	http.Handle("/movies/add", auth.RoleMiddleware("admin", http.HandlerFunc(movieHandler.HandleAddMovie)))        // POST /movies/add
-	http.Handle("/movies/update/", auth.RoleMiddleware("admin", http.HandlerFunc(movieHandler.HandleUpdateMovie))) // PUT /movies/update/{id}
-	http.Handle("/movies/delete/", auth.RoleMiddleware("admin", http.HandlerFunc(movieHandler.HandleDeleteMovie))) // DELETE /movies/delete/{id}
+	http.HandleFunc("/movies", movieHandler.HandleGetMovies)
+	http.Handle("/movies/add", auth.RoleMiddleware("admin", http.HandlerFunc(movieHandler.HandleAddMovie)))
+	http.Handle("/movies/update/", auth.RoleMiddleware("admin", http.HandlerFunc(movieHandler.HandleUpdateMovie)))
+	http.Handle("/movies/delete/", auth.RoleMiddleware("admin", http.HandlerFunc(movieHandler.HandleDeleteMovie)))
 
 	// User routes
-	http.HandleFunc("/auth/signup", authHandler.SignUp) // POST /auth/signup
-	http.HandleFunc("/auth/login", authHandler.LogIn)   // POST /auth/login
+	http.HandleFunc("/auth/signup", authHandler.SignUp)
+	http.HandleFunc("/auth/login", authHandler.LogIn)
 
 	// Showtime routes
-	http.HandleFunc("/showtimes", showtimeHandler.HandleGetShowtimes)                                                       // GET /showtimes
-	http.Handle("/showtimes/add", auth.RoleMiddleware("admin", http.HandlerFunc(showtimeHandler.HandleAddShowtime)))        // POST /showtimes/add
-	http.Handle("/showtimes/update/", auth.RoleMiddleware("admin", http.HandlerFunc(showtimeHandler.HandleUpdateShowtime))) // PUT /showtimes/update/{id}
-	http.Handle("/showtimes/delete/", auth.RoleMiddleware("admin", http.HandlerFunc(showtimeHandler.HandleDeleteShowtime))) // DELETE /showtimes/delete/{id}
-	http.HandleFunc("/showtimes/seats/", showtimeHandler.HandleGetSeats)                                                    // GET /showtimes/seats/{id}
+	http.HandleFunc("/showtimes", showtimeHandler.HandleGetShowtimes)
+	http.Handle("/showtimes/add", auth.RoleMiddleware("admin", http.HandlerFunc(showtimeHandler.HandleAddShowtime)))
+	http.Handle("/showtimes/update/", auth.RoleMiddleware("admin", http.HandlerFunc(showtimeHandler.HandleUpdateShowtime)))
+	http.Handle("/showtimes/delete/", auth.RoleMiddleware("admin", http.HandlerFunc(showtimeHandler.HandleDeleteShowtime)))
+	http.HandleFunc("/showtimes/seats/", showtimeHandler.HandleGetSeats)
 
 	// Reservation routes
-	http.Handle("/reserve/add", auth.RoleMiddleware("user", http.HandlerFunc(reservationHandler.HandleReservation)))                 // POST /reserve/add
-	http.HandleFunc("/reserve/delete/", reservationHandler.HandleCancelReservation)                                                  // DELETE /reserve/delete
-	http.HandleFunc("/reserve", reservationHandler.HandleGetReservations)                                                            // GET /reserve
-	http.Handle("/reserve/all", auth.RoleMiddleware("admin", http.HandlerFunc(reservationHandler.HandleGetAllReservations)))         // GET /reserve/all
-	http.Handle("/reserve/movie/", auth.RoleMiddleware("admin", http.HandlerFunc(reservationHandler.HandleGetReservationsPerMovie))) // GET /reserve/movie
+	http.Handle("/reserve/add", auth.RoleMiddleware("user", http.HandlerFunc(reservationHandler.HandleReservation)))
+	http.HandleFunc("/reserve/delete/", reservationHandler.HandleCancelReservation)
+	http.HandleFunc("/reserve", reservationHandler.HandleGetReservations)
+	http.Handle("/reserve/all", auth.RoleMiddleware("admin", http.HandlerFunc(reservationHandler.HandleGetAllReservations)))
+	http.Handle("/reserve/movie/", auth.RoleMiddleware("admin", http.HandlerFunc(reservationHandler.HandleGetReservationsPerMovie)))
 
 	// Revenue routes
-	http.Handle("/revenue", auth.RoleMiddleware("admin", http.HandlerFunc(reservationHandler.HandleGetTotalRevenue))) // GET /revenue
+	http.Handle("/revenue", auth.RoleMiddleware("admin", http.HandlerFunc(reservationHandler.HandleGetTotalRevenue)))
 }
