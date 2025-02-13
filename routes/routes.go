98980410739914a6fa5c3f40b95f1d1914593e9b
Eugene Martynov <tambov.j678@gmail.com>
2025-02-13 19:38:42 +0300

    Add Prometheus metrics middleware to routes

--- routes/routes.go
+++ routes/routes.go
@@ -3,34 +3,38 @@ package routes
 import (
 	"movie-system/internal/auth"
 	"movie-system/internal/handlers"
+	"movie-system/metrics"
 	"net/http"
 )
 
 func SetupRoutes(movieHandler *handlers.MovieHandler, showtimeHandler *handlers.ShowtimeHandler, authHandler *handlers.AuthHandler, reservationHandler *handlers.ReservationHandler) {
 	// Movie routes
-	http.HandleFunc("/movies", movieHandler.HandleGetMovies)
-	http.Handle("/movies/add", auth.RoleMiddleware("admin", http.HandlerFunc(movieHandler.HandleAddMovie)))
-	http.Handle("/movies/update/", auth.RoleMiddleware("admin", http.HandlerFunc(movieHandler.HandleUpdateMovie)))
-	http.Handle("/movies/delete/", auth.RoleMiddleware("admin", http.HandlerFunc(movieHandler.HandleDeleteMovie)))
+	http.HandleFunc("/movies", metrics.RequestCounter(movieHandler.HandleGetMovies))
+	http.Handle("/movies/add", auth.RoleMiddleware("admin", http.HandlerFunc(metrics.RequestCounter(movieHandler.HandleAddMovie))))
+	http.Handle("/movies/update/", auth.RoleMiddleware("admin", http.HandlerFunc(metrics.RequestCounter(movieHandler.HandleUpdateMovie))))
+	http.Handle("/movies/delete/", auth.RoleMiddleware("admin", http.HandlerFunc(metrics.RequestCounter(movieHandler.HandleDeleteMovie))))
 
 	// User routes
-	http.HandleFunc("/auth/signup", authHandler.SignUp)
-	http.HandleFunc("/auth/login", authHandler.LogIn)
+	http.HandleFunc("/auth/signup", metrics.RequestCounter(authHandler.SignUp))
+	http.HandleFunc("/auth/login", metrics.RequestCounter(authHandler.LogIn))
 
 	// Showtime routes
-	http.HandleFunc("/showtimes", showtimeHandler.HandleGetShowtimes)
-	http.Handle("/showtimes/add", auth.RoleMiddleware("admin", http.HandlerFunc(showtimeHandler.HandleAddShowtime)))
-	http.Handle("/showtimes/update/", auth.RoleMiddleware("admin", http.HandlerFunc(showtimeHandler.HandleUpdateShowtime)))
-	http.Handle("/showtimes/delete/", auth.RoleMiddleware("admin", http.HandlerFunc(showtimeHandler.HandleDeleteShowtime)))
-	http.HandleFunc("/showtimes/seats/", showtimeHandler.HandleGetSeats)
+	http.HandleFunc("/showtimes", metrics.RequestCounter(showtimeHandler.HandleGetShowtimes))
+	http.Handle("/showtimes/add", auth.RoleMiddleware("admin", http.HandlerFunc(metrics.RequestCounter(showtimeHandler.HandleAddShowtime))))
+	http.Handle("/showtimes/update/", auth.RoleMiddleware("admin", http.HandlerFunc(metrics.RequestCounter(showtimeHandler.HandleUpdateShowtime))))
+	http.Handle("/showtimes/delete/", auth.RoleMiddleware("admin", http.HandlerFunc(metrics.RequestCounter(showtimeHandler.HandleDeleteShowtime))))
+	http.HandleFunc("/showtimes/seats/", metrics.RequestCounter(showtimeHandler.HandleGetSeats))
 
 	// Reservation routes
-	http.Handle("/reserve/add", auth.RoleMiddleware("user", http.HandlerFunc(reservationHandler.HandleReservation)))
-	http.HandleFunc("/reserve/delete/", reservationHandler.HandleCancelReservation)
-	http.HandleFunc("/reserve", reservationHandler.HandleGetReservations)
-	http.Handle("/reserve/all", auth.RoleMiddleware("admin", http.HandlerFunc(reservationHandler.HandleGetAllReservations)))
-	http.Handle("/reserve/movie/", auth.RoleMiddleware("admin", http.HandlerFunc(reservationHandler.HandleGetReservationsPerMovie)))
+	http.Handle("/reserve/add", auth.RoleMiddleware("user", http.HandlerFunc(metrics.RequestCounter(reservationHandler.HandleReservation))))
+	http.HandleFunc("/reserve/delete/", metrics.RequestCounter(reservationHandler.HandleCancelReservation))
+	http.HandleFunc("/reserve", metrics.RequestCounter(reservationHandler.HandleGetReservations))
+	http.Handle("/reserve/all", auth.RoleMiddleware("admin", http.HandlerFunc(metrics.RequestCounter(reservationHandler.HandleGetAllReservations))))
+	http.Handle("/reserve/movie/", auth.RoleMiddleware("admin", http.HandlerFunc(metrics.RequestCounter(reservationHandler.HandleGetReservationsPerMovie))))
 
 	// Revenue routes
-	http.Handle("/revenue", auth.RoleMiddleware("admin", http.HandlerFunc(reservationHandler.HandleGetTotalRevenue)))
+	http.Handle("/revenue", auth.RoleMiddleware("admin", http.HandlerFunc(metrics.RequestCounter(reservationHandler.HandleGetTotalRevenue))))
+
+	// Metrics routes
+	http.Handle("/metrics", metrics.MetricsHandler())
 }
